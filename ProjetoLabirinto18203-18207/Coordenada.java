7e0c86c7da811efa780be9ff5b82172e992a5bdb
Danilo-Nunes <42344266+Danilo-Nunes@users.noreply.github.com>
2018-09-19 12:05:06 -0300

    aaa
    aaaa

--- ProjetoLabirinto18203-18207/Coordenada.java
+++ ProjetoLabirinto18203-18207/Coordenada.java
@@ -1,11 +1,57 @@
-static void main(String[] args)
+public class Coordenada
 {
-    int x = 0;
-    int y = 0;
+    private int x = 0;
+    private int y = 0;
 
-    public Coordenada(int x, int y)
+    public Coordenada(int x, int y) throws Exception
     {
+        if(x < 0 || y < 0 || x == null || y == null)
+            throw new Exception("Coordenadas inválidas! Digite-as corretamente!");
+
         this.x = x;
         this.y = y;
+    }
+
+    public String toString()
+    {
+        return "(" + this.x + ", " + this.y + ")";
+    }
+
+    public int getX()
+    {
+        return this.x;
+    }
+
+    public int getY()
+    {
+        return this.y;
+    }
+
+    public boolean equals(Object obj)
+    {
+        if(this == obj)
+            return true;
+        
+        if(obj == null)
+            return false;
+
+        if(this.getClass() != obj.getClass())
+            return false;
+
+        Coordenada coord = (Coordenada)obj;
+
+        if(this.x != obj.x || this.y != obj.y)
+            return false;
+
+        return true;
+    }
+
+    public int hashCode()
+    {
+        int ret = 1;
+
+        ret = ret*2 + new Integer(this.x).hashCode() + new Integer(this.y).hashCode();
+
+        return ret;
     }
 }
--- ProjetoLabirinto18203-18207/Program.java
+++ ProjetoLabirinto18203-18207/Program.java
@@ -1,4 +1,14 @@
-static void main(String[] args)
+import Pilha.*;
+import Fila.*;
+import Coordenada.*;
+import java.io.*;
+
+public class Program
 {
-    
+    //ver se é número
+    static void main(String[] args)
+    {
+        BufferedReader teclado = new BufferedReader(new InputStreamReader(System.in));
+        // corrigir convenções do java para simbolos
+    }
 }
