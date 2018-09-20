fbdb98fa21588db3f892e6ca355dec6b98093b43
JoaoJavitti <42344222+JoaoJavitti@users.noreply.github.com>
2018-09-20 08:49:24 -0300

    Métodos para checar labirinto

--- ProjetoLabirinto18203-18207/Program.java
+++ ProjetoLabirinto18203-18207/Program.java
@@ -17,6 +17,7 @@ public class Program
 
         int linhas = 0;
         int colunas = 0;
+        boolean taSerto;
 
         linhas = file.readLine();
         colunas = file.readLine();
@@ -27,10 +28,67 @@ public class Program
         for(int i = 0; i < linhas; i++)
             for(int j = 0; j < colunas; j++)
                 labirinto[i][j] = file.read();
+        file.close();
 
         Pilha<Coordenadas> caminho = new Pilha<Coordenadas>(linhas*colunas);
 
         Fila<Coordenadas> fila = new Fila<Coordenadas>(3);
         Pilha<Fila<Cordenadas>> possibilidades = new Pilha<Fila<Cordenadas>>(linhas*colunas);
+
+        if(checarE() && checarS())
+        {
+
+        }
+        else
+        {
+            throw new Exception('Entrada ou saída não encontradas');
+        }
+
+
+        private boolean checarE()
+        {
+            for(int i = 0; i < colunas; i++)
+                if(labirinto[0][i] == 'E')
+                    return true;
+
+            for(int i = 0; i < linhas; i++)
+                if(labirinto[i][0] == 'E')
+                    return true;
+            
+            for(int i = 0; i < colunas; i++)
+                if(labirinto[linhas][i] == 'E')
+                    return true;
+
+            for(int i = 0; i < linhas; i++)
+                if(labirinto[i][colunas] == 'E')
+                    return true;
+
+            return false;
+
+        }
+
+        private boolean checarS()
+        {
+            for(int i = 0; i < colunas; i++)
+                if(labirinto[0][i] == 'S')
+                    return true;
+
+            for(int i = 0; i < linhas; i++)
+                if(labirinto[i][0] == 'S')
+                    return true;
+            
+            for(int i = 0; i < colunas; i++)
+                if(labirinto[linhas][i] == 'S')
+                    return true;
+
+            for(int i = 0; i < linhas; i++)
+                if(labirinto[i][colunas] == 'S')
+                    return true;
+
+            return false;
+
+        }
+
+                
     }
 }
